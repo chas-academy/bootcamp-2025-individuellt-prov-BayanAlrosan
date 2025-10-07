f828fc4af5c3ac96daf9bdaaa7959f237094e19b
BayanAlrosan <bayan.alrosan@chasacademy.se>
2025-10-07 12:37:33 +0200

    Slutversion av individuellt prov

--- q8.js
+++ q8.js
@@ -1,8 +1,10 @@
 // denna funktion tar emot en array av strängar. gör så att den returnerar en enda sträng som sammanfogar alla strängar i arrayen
 
+import { findSmallerNumber } from "./q7";
+
 export function combineStrings(strings) {
   let result = "";
-  strings.forEach((string) => {});
-
+  strings.forEach((string) => {result+= string; 
+  });
   return result;
 }
